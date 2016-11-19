a96a8a4c53510413f2cec120b2a9f6dcecfda475
unknown <duy.t.dang@gmail.com>
2016-11-19 02:53:21 -0600

    First

---
+++ server.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var app = express();
+
+/*app.get('/', function(req,res){
+    res.send('hello Binan');
+    
+});*/
+
+app.use(express.static(__dirname + '/public'));
+
+var ip = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
+
+app.listen(port, ip);
